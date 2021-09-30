067f01ca9ca330a61a167a3467251b89f9f9f826
accranmer <85885052+accranmer@users.noreply.github.com>
2021-09-30 09:18:06 -0500

    Update graphnode.cpp

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -10,9 +10,7 @@ GraphNode::~GraphNode()
 {
     //// STUDENT CODE
     ////
-
-    delete _chatBot; 
-
+    //    delete _chatBot; // Task 0
     ////
     //// EOF STUDENT CODE
 }
@@ -27,23 +25,29 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
     _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+//void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge) // Task 4
 {
-    _childEdges.push_back(edge);
+    _childEdges.push_back(std::move(edge)); // Task 4
+    //_childEdges.push_back(std::unique_ptr<GraphEdge> (edge)); // Task 4
 }
 
 //// STUDENT CODE
 ////
-void GraphNode::MoveChatbotHere(ChatBot *chatbot)
+//void GraphNode::MoveChatbotHere(ChatBot *chatbot)
+void GraphNode::MoveChatbotHere(ChatBot chatbot) // Task 5
 {
-    _chatBot = chatbot;
-    _chatBot->SetCurrentNode(this);
+
+    _chatBot = std::move(chatbot);  // Task 5
+    //_chatBot->SetCurrentNode(this);
+    _chatBot.SetCurrentNode(this);
+    
 }
 
 void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
 {
-    newNode->MoveChatbotHere(_chatBot);
-    _chatBot = nullptr; // invalidate pointer at source
+    newNode->MoveChatbotHere(std::move(_chatBot)); // Task 5
+    //_chatBot = nullptr; // invalidate pointer at source  // Task 5
 }
 ////
 //// EOF STUDENT CODE
@@ -53,8 +57,8 @@ GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
     //// STUDENT CODE
     ////
 
-    return _childEdges[index];
-
+    //return _childEdges[index];
+    return (_childEdges[index]).get(); // Task 4
     ////
     //// EOF STUDENT CODE
 }
